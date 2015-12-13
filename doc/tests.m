e55b52d39ee15df2e7497d99227094bcd4d35212
Travis Collins <travisfcollins@gmail.com>
2015-12-13 11:30:04 -0500

    move to cmake with travis-ci, added tests doc.

---
+++ doc/tests.m
@@ -0,0 +1,10 @@
+%% Testing
+% Current testing only builds and runs against C++ code.
+% MATLAB testing doesn't really work with travis-ci, so
+% we just look at the C++ code.
+%
+% Current Tests:
+% - TestFlow: 2 block test (1 sink, 1 source), just to 
+%   make sure everthing will compile, run, and exit w/o error
+%   -- File: src/testFlowMP.cpp
+%
