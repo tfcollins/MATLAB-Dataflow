a7d784a6e3084ad0191357140d79230c7322737f
Travis Collins <travisfcollins@gmail.com>
2017-01-30 19:38:07 -0500

    Update testGraphGenerator.m
    Updated test/example code

--- mfiles/testGraphGenerator.m
+++ mfiles/testGraphGenerator.m
@@ -16,12 +16,10 @@ g.Connect('awgn_0',1,'filter_0',0)
 g.Connect('filter_0',0,'sink_0',1)
 
 g.DrawGraph('simple_with_channel.dot');
-
-
 pause(4);
 
 %% FFT Example
-% Construct graph and build all code
+% Construct graph, build all code, and show graph
 clear all;close all;
 g = GraphGenerator;
 g.SimDurationSec = 30;
@@ -38,30 +36,24 @@ g.Connect('FFT2',0,'DS',0);
 
 limit = 100;
 g.WriteCode(limit);
-
 g.DrawGraph('simple_with_channel.dot');
-
 g.Build();
 
-%% Construct graph and build all code
+%% CRC Example
+% Construct graph and build all code
 clear all;close all;
 g = GraphGenerator;
 g.SimDurationSec = 30;
 
 g.AddBlock('GenCRC',0,1);
-%g.AddBlock('AddNoise',1,1);
+g.AddBlock('AddNoise',1,1);
 g.AddBlock('CheckCRC',1,0);
 
-%g.Connect('GenCRC',0,'AddNoise',0);
-%g.Connect('AddNoise',0,'CheckCRC',0);
-g.Connect('GenCRC_0',0,'CheckCRC_0',0);
+g.Connect('GenCRC',0,'AddNoise',0);
+g.Connect('AddNoise',0,'CheckCRC',0);
 
 limit = 100;
 g.WriteCode(limit);
-
 g.DrawGraph('simple_with_channel.dot');
-
 g.Build();
-
-%!./RX
-%Graph(limit)
+g.Run();
